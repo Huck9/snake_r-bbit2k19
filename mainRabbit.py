d0de080f4980524b2f4ee843000b4fa47cb8cf30
kacper <k.zarnicki@gmail.com>
2019-04-02 19:35:44 +0200

    First commit

---
+++ mainRabbit.py
@@ -0,0 +1,53 @@
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+
+class cube(object):
+    rows = 0
+    w = 0
+
+    def __init__(self, start, dirx = 1, diry = 0, color = (255,0,0)):
+        pass
+
+    def move(self, dirx,diry):
+        pass
+
+    def draw(self, surface):
+        pass
+
+class snake(object):
+    def __init__(self, color, pos):
+        pass
+
+    def move(self):
+        pass
+
+    def reset(self, pos):
+        pass
+
+    def addCube(self):
+        pass
+
+    def draw(self, surfece):
+        pass
+
+
+def drawGrid(w, rows, surface):
+    pass
+
+def redrawWindow(surface):
+    pass
+
+def randomSnack(row, item):
+    pass
+
+def message_box(subject, content):
+    pass
+
+def main():
+    pass
+
+
+main()
